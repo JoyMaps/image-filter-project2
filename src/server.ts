4acc4a43f0dd057cd4bbb46dc84d9cb99f40da38
JoyMaps <joylinemapangire@gmail.com>
2022-08-26 23:36:05 +0200

    Creating endpoint for filtered image

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+app.get("/filteredimage", async ( req, res ) =>{
+const image_url = req.query.image_url.toString();
+const filteredimage = await filterImageFromURL(image_url);
+res.status(200).sendFile(filteredimage, () =>{
+  deleteLocalFiles([filteredimage]);
+});
 
+});
   //! END @TODO1
   
   // Root Endpoint
