9b27d86809962faa9b24e3555e9a5fe352353087
JoyMaps <joylinemapangire@gmail.com>
2022-09-01 23:50:32 +0200

    more changes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
